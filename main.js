42836991fab9b682496976d178b599f85765fd53
Khan From Chedum <khanfromchedum@elsewhere.com>
2021-05-11 19:30:58 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
+
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,     //In order to get access to ipc renderer 
+      contextIsolation: false   
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
